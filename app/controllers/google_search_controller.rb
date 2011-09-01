02b8d942dd7874c093d1a70528a6a53db426cc0c
Thiago <thiago@oficina7.com>
2011-09-01 20:20:03 -0300

    google search cached for a day :)

--- app/controllers/google_search_controller.rb
+++ app/controllers/google_search_controller.rb
@@ -3,24 +3,27 @@ class GoogleSearchController < ApplicationController
   
   def search
     text = params[:text]
-    GoogleAjax.referer = 'executter.com'
-    @image_list = GoogleAjax::Search.images(text)
-    @video_list = GoogleAjax::Search.video(text)
-    @text_list = []
-    #news_list  = GoogleAjax::Search.news(text)
-    #web_list   = GoogleAjax::Search.web(text)
-    #blog_list  = GoogleAjax::Search.blogs(text)
-    GoogleAjax::Search.web(text)[:results].each do |web|
-      @text_list << {:url=>web[:url],     :text=>web[:content]}
-    end
-    GoogleAjax::Search.news(text)[:results].each do |news|
-      @text_list << {:url=>news[:url],      :text=>news[:title]}
-    end
-    GoogleAjax::Search.blogs(text)[:results].each do |blog|
-      @text_list << {:url=>blog[:post_url], :text=>blog[:title]}
-    end
-    GoogleAjax::Search.books(text)[:results].each do |book|
-      @text_list << {:url=>book[:url],      :text=>book[:title]}
+    @cache_key = [:google, :search, text]
+    unless fragment_exist? @cache_key
+      GoogleAjax.referer = 'executter.com'
+      @image_list = GoogleAjax::Search.images(text)
+      @video_list = GoogleAjax::Search.video(text)
+      @text_list = []
+      #news_list  = GoogleAjax::Search.news(text)
+      #web_list   = GoogleAjax::Search.web(text)
+      #blog_list  = GoogleAjax::Search.blogs(text)
+      GoogleAjax::Search.web(text)[:results].each do |web|
+        @text_list << {:url=>web[:url],     :text=>web[:content]}
+      end
+      GoogleAjax::Search.news(text)[:results].each do |news|
+        @text_list << {:url=>news[:url],      :text=>news[:title]}
+      end
+      GoogleAjax::Search.blogs(text)[:results].each do |blog|
+        @text_list << {:url=>blog[:post_url], :text=>blog[:title]}
+      end
+      GoogleAjax::Search.books(text)[:results].each do |book|
+        @text_list << {:url=>book[:url],      :text=>book[:title]}
+      end
     end
   end
 
