6d552eef355dee48db8e939d244349d5b9478e45
Thiago <thiago@oficina7.com>
2011-09-02 16:09:16 -0300

    sep 2nd. #7 bug fix reported by exceptional (this is just a rescue)

--- app/controllers/google_search_controller.rb
+++ app/controllers/google_search_controller.rb
@@ -2,28 +2,21 @@ class GoogleSearchController < ApplicationController
   sub_layout 'triple'
   
   def search
-    text = params[:text]
-    @cache_key = [:google, :search, text]
-    unless fragment_exist? @cache_key
-      GoogleAjax.referer = 'executter.com'
-      @image_list = GoogleAjax::Search.images(text)
-      @video_list = GoogleAjax::Search.video(text)
-      @text_list = []
-      #news_list  = GoogleAjax::Search.news(text)
-      #web_list   = GoogleAjax::Search.web(text)
-      #blog_list  = GoogleAjax::Search.blogs(text)
-      GoogleAjax::Search.web(text)[:results].each do |web|
-        @text_list << {:url=>web[:url],     :text=>web[:content]}
+    begin
+      redirect_to :root unless text = params[:text]
+      @cache_key = [:google, :search, text]
+      unless fragment_exist? @cache_key
+        GoogleAjax.referer = 'executter.com'
+        @image_list = GoogleAjax::Search.images(text)
+        @video_list = GoogleAjax::Search.video(text)
+        @text_list = []
+        GoogleAjax::Search.web(text)[:results].each   { |r| @text_list << {:url=>r[:url],       :text=>r[:content]}  }
+        GoogleAjax::Search.news(text)[:results].each  { |r| @text_list << {:url=>r[:url],       :text=>r[:title]}    }
+        GoogleAjax::Search.blogs(text)[:results].each { |r| @text_list << {:url=>r[:post_url],  :text=>r[:title]}    }
+        GoogleAjax::Search.books(text)[:results].each { |r| @text_list << {:url=>r[:url],       :text=>r[:title]}    }
       end
-      GoogleAjax::Search.news(text)[:results].each do |news|
-        @text_list << {:url=>news[:url],      :text=>news[:title]}
-      end
-      GoogleAjax::Search.blogs(text)[:results].each do |blog|
-        @text_list << {:url=>blog[:post_url], :text=>blog[:title]}
-      end
-      GoogleAjax::Search.books(text)[:results].each do |book|
-        @text_list << {:url=>book[:url],      :text=>book[:title]}
-      end
+    rescue
+      redirect_to :root
     end
   end
 
