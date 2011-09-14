a841ddd2a05855fa30a8eba86f0b392bfead9399
Thiago <thiago@oficina7.com>
2011-09-14 00:05:14 -0300

    smiles visible by default

--- db/migrate/20110914020932_create_smiles.rb
+++ db/migrate/20110914020932_create_smiles.rb
@@ -2,7 +2,7 @@ class CreateSmiles < ActiveRecord::Migration
   def self.up
     create_table :smiles do |t|
       t.string :key
-      t.boolean :is_visible, :default=>false
+      t.boolean :is_visible, :default=>true
 
       t.timestamps
     end
